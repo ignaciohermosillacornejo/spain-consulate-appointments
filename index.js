a7501c7d99924b37b06bbc60558db643f07b915f
Ignacio Hermosilla <nach@fb.com>
2024-07-07 17:31:18 -0500

    make sure browser is closed even if it errors

--- index.js
+++ index.js
@@ -11,60 +11,63 @@ const maxRetries = 3;
 dotenv.config({ path: path.resolve(process.cwd(), `.env.${env}`) });
 
 const checkAppointmentAvailability = async () => {
-     // Launch the browser
-     const browser = await launch({ headless: process.env.headless || true});
-     const page = await browser.newPage();
+    try {
+        // Launch the browser
+        const browser = await launch({ headless: process.env.headless || true});
+        const page = await browser.newPage();
 
-     // Navigate to the URL
-     await page.goto('https://www.exteriores.gob.es/Consulados/santiagodechile/es/ServiciosConsulares/Paginas/CitaPrevia.aspx');
+        // Navigate to the URL
+        await page.goto('https://www.exteriores.gob.es/Consulados/santiagodechile/es/ServiciosConsulares/Paginas/CitaPrevia.aspx');
 
-     // Click on the "Solicitud de Cita" link
-     const citaLink = 'https://www.citaconsular.es/es/hosteds/widgetdefault/24e329baaaf932b93de4fc7f95bc53535';
-     await page.waitForSelector(`a[href="${citaLink}"]`);
-     await page.click(`a[href="${citaLink}"]`);
+        // Click on the "Solicitud de Cita" link
+        const citaLink = 'https://www.citaconsular.es/es/hosteds/widgetdefault/24e329baaaf932b93de4fc7f95bc53535';
+        await page.waitForSelector(`a[href="${citaLink}"]`);
+        await page.click(`a[href="${citaLink}"]`);
 
+        // Wait for the new page or tab to load
+        const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
+        const newPage = await newPagePromise;
+        // Handle the alert by accepting it on the new page
+        newPage.on('dialog', async dialog => {
+            await dialog.accept();
+        });
+        // Wait for the button to be available and visible
+        await newPage.waitForSelector('#idCaptchaButton', { visible: true });
+        // Click on the "Continue / Continuar" button
+        await newPage.click('#idCaptchaButton');
 
-     // Wait for the new page or tab to load
-     const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
-     const newPage = await newPagePromise;
-     // Handle the alert by accepting it on the new page
-     newPage.on('dialog', async dialog => {
-         await dialog.accept();
-     });
-     // Wait for the button to be available and visible
-     await newPage.waitForSelector('#idCaptchaButton', { visible: true });
-     // Click on the "Continue / Continuar" button
-     await newPage.click('#idCaptchaButton');
+        // Check for availability
+        await newPage.waitForSelector('#idDivBktServicesContainer > div:nth-child(2)', { visible: true });
+        const noAvailability = await newPage.evaluate(() => {
+            const widget = document.querySelector('#idDivBktServicesContainer > div:nth-child(2)');
+            return widget && widget.textContent.includes('No hay horas disponibles.');
+          });
 
-     // Check for availability
-     await newPage.waitForSelector('#idDivBktServicesContainer > div:nth-child(2)', { visible: true });
-     const noAvailability = await newPage.evaluate(() => {
-         const widget = document.querySelector('#idDivBktServicesContainer > div:nth-child(2)');
-         return widget && widget.textContent.includes('No hay horas disponibles.');
-       });
+       await browser.close();
 
-    await browser.close();
-
-    return noAvailability;
+       return noAvailability;
+    } catch (error) {
+        browser.close(); // make sure we close even if we error, to avoid OOM
+        throw error;
+    }
 }
 
 
 const checkAppointmentAvailabilityAndNotify = async (maxRetries) => {
-    const startTime = Date.now();
     let retries = maxRetries;
+    let startTime;
     while (retries > 0) {
         try {
+            startTime = Date.now();
             console.log(`Starting attempt #${maxRetries - retries + 1}`);
             const noAvailability = await checkAppointmentAvailability();
             if (!noAvailability) {
                 console.log('Appointment slots available!');
                 await sendPushoverNotification(sucessMessage, 'Appointment Alert', 2);
-                retries = 0;
                 break; // Exit the loop if successful
             } else {
                 console.log('No appointment slots available.');
                 await sendPushoverNotification(failureMessage, 'Appointment Alert', -2);
-                retries = 0;
                 break; // Exit the loop if unsuccessful
             }
         } catch (error) {
@@ -78,10 +81,11 @@ const checkAppointmentAvailabilityAndNotify = async (maxRetries) => {
                 console.error('An unexpected error occurred:', error);
                 throw error;
             }
+        } finally {
+            const endTime = Date.now();
+            console.log(`Time taken: ${(endTime - startTime) / 1000} seconds`);
         }
     }
-    const endTime = Date.now();
-    console.log(`Time taken: ${(endTime - startTime) / 1000} seconds`);
 }
 
 
